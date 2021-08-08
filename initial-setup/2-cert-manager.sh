6384f0393a5d734517ebf735914625259e357cec
Julius Tens <mail@juliustens.eu>
2021-08-08 22:56:08 +0200

    update cert manager: 0.16.1 -> 1.4.3

--- initial-setup/2-cert-manager.sh
+++ initial-setup/2-cert-manager.sh
@@ -3,7 +3,7 @@ set -e # exit on error
 
 kubectl create namespace cert-manager
 
-kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.16.1/cert-manager.yaml
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.4.3/cert-manager.yaml
 
 # cert-manager pods should have been created
 kubectl get pods --namespace cert-manager
