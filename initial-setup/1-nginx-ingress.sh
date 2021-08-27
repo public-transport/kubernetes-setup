85d49857ff9e2131a66eae9c370e8534dac0d718
Julius Tens <mail@juliustens.eu>
2021-08-27 12:33:03 +0200

    update nginx and cert-manager to k8s 1.22: use new ingress api

--- initial-setup/1-nginx-ingress.sh
+++ initial-setup/1-nginx-ingress.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e # exit on error
 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/mandatory.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/provider/cloud-generic.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.0/deploy/static/provider/scw/deploy.yaml
 
 # controller pods should have been created
 kubectl get pods --all-namespaces -l app.kubernetes.io/name=ingress-nginx
